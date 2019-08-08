250cc018c8fac9e1d6d8910d631273506c7abef2
Nima Askari ----- نیما عسکری <nimaltd@yahoo.com>
2019-08-08 10:26:32 +0430

    Add files via upload

--- FingerPrintConfig.h
+++ FingerPrintConfig.h
@@ -14,10 +14,11 @@ SM25 Fingerprint Library For STM32
  2)Enable a gpio as input for Finger detection pin.
  3)Enable USART and Enable RX interrupt(115200 default module).
  4)Change Selected USART to LL Library.
- 5)Config your Board on "FingerprintConfig.h"
- 6)Put FP_RxCallBack() function to your Interrupt routin on "stm32fxxx_it.c".
- 7)Put FP_Loop() into your "while" on Mani Function.
- 8)Call FP_Init().
+ 5)Select "General peripheral Initalizion as a pair of '.c/.h' file per peripheral" on project settings. 
+ 6)Config your Board on "FingerprintConfig.h"
+ 7)Put FP_RxCallBack() function to your Interrupt routin on "stm32fxxx_it.c".
+ 8)Put FP_Loop() into your "while" on Mani Function.
+ 9)Call FP_Init().
 */
 
 #define _FP_USART                     USART6
